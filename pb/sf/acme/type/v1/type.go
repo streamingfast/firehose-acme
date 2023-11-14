7422434f67bb1ff2aaf9968f1d0e4f4fdc654c32
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2023-11-14 08:41:33 -0500

    bump firehose-core to v0.2.3

--- pb/sf/acme/type/v1/type.go
+++ pb/sf/acme/type/v1/type.go
@@ -12,12 +12,22 @@ func (b *Block) GetFirehoseBlockNumber() uint64 {
 	return b.Height
 }
 
+func (b *Block) GetFirehoseBlockParentNumber() uint64 {
+	// This needs to be adapted for your own chain rules!
+	return b.Height - 1
+}
+
 func (b *Block) GetFirehoseBlockParentID() string {
 	return b.PrevHash
 }
 
 func (b *Block) GetFirehoseBlockTime() time.Time {
 	return time.Unix(0, int64(b.Timestamp)).UTC()
+}
+
+func (b *Block) GetFirehoseBlockVersion() int32 {
+	// This needs to be adapted for your own version used in pbbstream
+	return 0
 }
 
 func (b *Block) GetFirehoseBlockLIBNum() uint64 {
